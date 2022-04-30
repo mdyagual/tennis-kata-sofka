3df1be86df5d6ac085b9f5be4eb20278e0bd5a34
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-04-29 23:23:23 -0500

    Refactorización Tenis 1

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,8 +1,8 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int scoreP1 = 0;
+    private int scoreP2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -12,65 +12,76 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals(player1Name))
+            scoreP1 += 1;
         else
-            m_score2 += 1;
+            scoreP2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (esEmpate())
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+           score = puntajesIguales();
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (esMayorA4())
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+           score = puntajeMayor();
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score=puntajes(scoreP1).concat("-").concat(puntajes(scoreP2));
         }
         return score;
     }
+
+    //Booleanos
+    private boolean esEmpate(){
+        return scoreP1 == scoreP2;
+    }
+
+    private boolean esMayorA4(){
+        return scoreP1 >= 4 || scoreP2 >=4;
+    }
+
+    //Switches
+    private String puntajes(int puntaje){
+        switch (puntaje){
+            case 0: return "Love";
+            case 1: return "Fifteen";
+            case 2: return "Thirty";
+            default: return "Forty";
+        }
+    }
+
+    private String puntajesIguales(){
+        switch (scoreP1){
+            case 0: return "Love-All";
+            case 1: return "Fifteen-All";
+            case 2: return "Thirty-All";
+            default: return "Deuce";
+        }
+    }
+
+    //Messages
+    private String ventajaEnPuntos(int puntos){
+        return puntos == 1 ? "Advantage ".concat(player1Name) : "Advantage ".concat(player2Name);
+    }
+
+    private String ganadorPuntos(int puntos){
+        return puntos >= 2 ? "Win for ".concat(player1Name) : "Win for ".concat(player2Name);
+    }
+
+    private String puntajeMayor(){
+        int puntos = scoreP1-scoreP2;
+        switch (puntos){
+            case 1: case -1: return ventajaEnPuntos(puntos);
+            default: return ganadorPuntos(puntos);
+        }
+    }
+
+    private String nombreJugador(){
+        return  scoreP1> scoreP2 ? player1Name : player2Name;
+    }
+
 }
